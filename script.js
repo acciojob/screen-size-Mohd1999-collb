36f509aeb82271875e50e7ec6103780abc8da4b0
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-08-22 01:22:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const heading = document.getElementsByTagName("h1")[0];
+
+
+window.addEventListener('resize', function(){
+	const width = window.innerWidth, height = window.innerHeight;
+	heading.innerText = `Width: ${width} and Height: ${height}`
+})
