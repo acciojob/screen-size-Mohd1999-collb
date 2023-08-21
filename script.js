f851fc7983de831484a6ddd2c9daa5ef8974322b
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-08-22 01:34:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-//your JS code here. If required.
-const heading = document.getElementsByTagName("h1")[0];
+// //your JS code here. If required.
+// const heading = document.getElementsByTagName("h1")[0];
 
 
-window.addEventListener('resize', function(){
-	const width = window.innerWidth, height = window.innerHeight;
-	heading.innerText = `Width: ${width} and Height: ${height}`
-})
+// window.addEventListener('resize', function(){
+// 	const width = window.innerWidth, height = window.innerHeight;
+// 	heading.innerText = `Width: ${width} and Height: ${height}`
+// })
+
+function updateSize() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const sizeInfoElement = document.querySelector('#sizeInfo h1');
+  sizeInfoElement.textContent = `Width: ${width} and Height: ${height}`;
+}
+
+// Initial call to set the initial size
+updateSize();
+
+// Add a resize event listener to update the size on window resize
+window.addEventListener('resize', updateSize);
